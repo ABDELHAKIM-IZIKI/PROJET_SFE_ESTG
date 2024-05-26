4e69a4f1222bbd48bc33595f3f0b0af194f05811
ABDELHAKIM-IZIKI <iziki.abdelhakim@gmail.com>
2024-05-27 00:36:03 +0100

    test

--- app/Http/Controllers/GestionnairesStockController.php
+++ app/Http/Controllers/GestionnairesStockController.php
@@ -1,86 +1,211 @@
-@extends('gestionnairestock.templateGS')
+<?php
 
-@section('title')
-Modifier le matériel ou l'équipement :
-@endsection
+namespace App\Http\Controllers;
 
-@section('content')
-<div class="p-4 bg-gray-300">
-    <form method="POST" action="{{ route('gestionnairestock.edit') }}" class="max-w-sm mx-auto" enctype="multipart/form-data">
-        @csrf
+use App\Http\Requests\MaterielRequest;
+use App\Http\Requests\MaterielRequestUpdate;
+use App\Models\Caracteristique;
+use App\Models\Categorie;
+use App\Models\Etat;
+use App\Models\Marque;
+use App\Models\Materiel;
+use App\Models\Registre;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File as FacadesFile;
+use Illuminate\Support\Facades\Storage;
 
-        <input value="{{ $materiels->id }}" type="hidden" name="id"/>
+class GestionnairesStockController extends Controller
+{
+    public function index()
+{
+    $materiels = Materiel::paginate(8);
+    $categories = Categorie::orderBy('nom')->get();
+    $marques =Marque::orderBy('nom')->get();
+    $dispoTab=Registre::select('materiels_id', Registre::raw('count(*) as a'))
+    ->groupBy('materiels_id')
+    ->get();
 
-        <!-- Other input fields... -->
+        return view('gestionnairestock.home', ['materiels' =>$materiels ,'marques'=>$marques ,'categories'=>$categories ,   'dispoTab' =>  $dispoTab ]);
+}
 
-        <div class="mb-5 mr-2">
-            <label class="block mb-2 text-sm font-medium text-black">Caractéristiques : <span class="text-red-600">*</span></label>
-            <div class="overflow-x-auto">
-                <table class="w-full text-sm text-left rtl:text-right text-gray-500">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-400">
-                        <tr>
-                            <th scope="col" class="px-6 py-3">Nom</th>
-                            <th scope="col" class="px-6 py-3">Valeur</th>
-                            <th scope="col" class="px-6 py-3">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody id="caracteristiques" class="bg-gray-100 border-gray-600 ">
-                        @foreach ($materiels->caracteristiques as $item)
-                        <tr>
-                            <td class="p-1 text-black">
-                                <input type="hidden" name="caracteristiques[{{ $loop->index }}][id]" value="{{ $item['id'] }}">
-                                <input type="text" name="caracteristiques[{{ $loop->index }}][nom]" placeholder="Nom de la caractéristique" value="{{ $item['nom'] }}" class="p-2 rounded-lg h-10 bg-gray-100 border-gray-600 placeholder-gray-700 text-gray-700" required>
-                            </td>
-                            <td class="p-1 text-black">
-                                <input type="text" name="caracteristiques[{{ $loop->index }}][valeur]" placeholder="Valeur de la caractéristique" value="{{ $item['valeur'] }}" class="p-2 rounded-lg h-10 bg-gray-100 border-gray-600 placeholder-gray-700 text-gray-700" required>
-                            </td>
-                            <td class="p-1 items-center flex justify-center">
-                                <button type="button" onclick="deleteCaracteristique(this)" class="rounded-lg p-2 bg-red-600 hover:bg-red-700">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
-                                </button>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            <div class="m-3 mr-0 float-right">
-                <button type="button" onclick="addCaracteristique()" class="rounded-lg h-10 px-4 py-2 w-25 bg-blue-600 hover:bg-blue-700 text-white">Ajouter une caractéristique</button>
-            </div>
-        </div>
-        <script>
-            let caracteristiqueIndex = {{ $materiels->caracteristiques->count() }};
 
-            function addCaracteristique() {
-                const caracteristiquesTbody = document.getElementById('caracteristiques');
-                const newCaracteristique = document.createElement('tr');
-                newCaracteristique.innerHTML = `
-                    <td class="p-1 text-black">
-                        <input type="hidden" name="caracteristiques[${caracteristiqueIndex}][id]" value="">
-                        <input type="text" name="caracteristiques[${caracteristiqueIndex}][nom]" placeholder="Nom de la caractéristique" class="p-2 rounded-lg h-10 bg-gray-100 border-gray-600 placeholder-gray-700 text-gray-700" required>
-                    </td>
-                    <td class="p-1 text-black">
-                        <input type="text" name="caracteristiques[${caracteristiqueIndex}][valeur]" placeholder="Valeur de la caractéristique" class="p-2 rounded-lg h-10 bg-gray-100 border-gray-600 placeholder-gray-700 text-gray-700" required>
-                    </td>
-                    <td class="p-1 items-center flex justify-center">
-                        <button type="button" onclick="deleteCaracteristique(this)" class="rounded-lg p-2 bg-red-600 hover:bg-red-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
-                        </button>
-                    </td>
-                `;
-                caracteristiquesTbody.appendChild(newCaracteristique);
-                caracteristiqueIndex++;
-            }
+    public function search(Request $r)
+{
+  
+  $q = Materiel::query(); 
 
-            function deleteCaracteristique(button) {
-                const row = button.parentElement.parentElement;
-                row.remove();
-            }
-        </script>
+  
+  if($r->has('categories_id') && ($r->input('categories_id') != 'Choisir une categorie')){
+      $v = $r->input('categories_id');
+      $q->where('categories_id','=', $v );
+  }
 
-        <div class="float-right m-5">
-            <button type="submit" class="rounded-lg h-10 px-4 py-2 w-25 mx-2 bg-blue-600 hover:bg-blue-700 text-white">Modifié</button>
-        </div>
-    </form>
-</div>
-@endsection
+  if($r->has('marques_id') && ($r->input('marques_id') != 'Choisir une marque')){
+      $v = $r->input('marques_id');
+      $q->where('marques_id','=', $v );
+  }
+
+  if($r->has('valeur') && ($r->input('valeur') != null)){
+      $v = $r->input('valeur');
+      $q->where('nom','like','%'.$v.'%')->orWhere('model','like','%'.$v.'%')->orWhere('barcode','like','%'.$v.'%');
+  }
+
+  
+
+$categories = Categorie::orderBy('nom')->get();
+$marques =Marque::orderBy('nom')->get();
+$materiels=$q->paginate(10);
+$dispoTab=Registre::select('materiels_id', Registre::raw('count(*) as a'))
+->groupBy('materiels_id')
+->get();
+
+
+return view('gestionnairestock.home', ['materiels' =>$materiels,'marques'=>$marques ,'categories'=>$categories , 'dispoTab' =>  $dispoTab  ]);
+
+}
+
+
+public function destroy(Request $r)
+  {
+    $destroyMateriel=Materiel::find($r['id']);
+    
+    $filePath = str_replace('/storage/', '', $destroyMateriel->image);
+    Storage::disk('public')->delete($filePath );
+    $destroyMateriel->delete();
+    return redirect()->back()->with('success','Supprimé avec succès');
+  }
+
+  public function fill()
+  {
+    $categories = Categorie::orderBy('nom')->get();
+    $marques =Marque::orderBy('nom')->get();
+    return view('gestionnairestock.ajouter',['materiels'=> null,'marques'=>$marques ,'categories'=>$categories ,'Caracteristique' => null ]);
+  }
+
+  public function create(MaterielRequest $a)
+  {
+    $r=$a->validated();
+    
+    $fileName=$a->file('image')->getClientOriginalName();
+    $filePath = $a->file('image')->storeAs('uploads', $fileName, 'public');
+
+    $materiel = Materiel::create([
+        'nom'=> $r['nom'],
+        'model'=>$r['model'],
+        'description'=>$r['description'],
+        'quantite'=>$r['quantite'],
+        'barcode'=>$r['barcode'],
+        'date'=>$r['date'],
+        'categories_id' =>$r['categories_id'] ,
+        'marques_id'=>$r['marques_id'] ,
+        'image'=> "/storage"."/".$filePath 
+      ]); 
+
+
+      foreach($r['caracteristiques'] as $item ){
+       Caracteristique::insert([
+        'materiels_id' =>  $materiel->id ,
+        'nom' =>  $item['nom'] ,
+        'valeur' =>  $item['valeur'] 
+       ]);
+
+      }
+      
+
+      return redirect()->route('GestionnairesStock.index')
+      ->with('success','Ajouté avec succès');
+  }
+
+
+  public function fillEdit(Request $r)
+  { 
+    $materiel=Materiel::find($r['id']);
+    $categories = Categorie::orderBy('nom')->get();
+    $marques =Marque::orderBy('nom')->get();
+    return view('gestionnairestock.modifie',['materiels'=> $materiel ,'marques'=>$marques ,'categories'=>$categories ]);
+  }
+
+
+  public function edit(MaterielRequestUpdate $a)
+  {  
+    $r=$a->validated();
+   
+    $materiel=Materiel::find($r['id']);
+    if($a->has('image')){
+     
+     $fileName=$a->file('image')->getClientOriginalName();
+     $filePath = $a->file('image')->storeAs('uploads', $fileName, 'public');
+
+     $materiel->categories_id=$r['categories_id'];
+     $materiel->marques_id=$r['marques_id'];
+     $materiel->save();
+
+     $materiel->update([
+      'nom'=> $r['nom'],
+      'model'=>$r['model'],
+      'description'=>$r['description'],
+      'quantite'=>$r['quantite'],
+      'barcode'=>$r['barcode'],
+      'date'=>$r['date'],
+      'image'=> "/storage"."/".$filePath 
+    ]);
+ 
+    return redirect()->route('GestionnairesStock.index')
+      ->with('success','modifié avec succès');
+    }else{
+      
+      $materiel->categories_id=$r['categories_id'];
+      $materiel->marques_id=$r['marques_id'];
+      $materiel->save();
+
+      $materiel->update([
+        'nom'=> $r['nom'],
+        'model'=>$r['model'],
+        'description'=>$r['description'],
+        'quantite'=>$r['quantite'],
+        'barcode'=>$r['barcode'],
+        'date'=>$r['date'],
+      ]);
+   
+
+      if($a->has('caracteristiques')){
+        foreach($a['caracteristiques'] as $item ){
+          
+          Caracteristique::insert([
+           'materiels_id' =>  $materiel->id ,
+           'nom' =>  $item['nom'] ,
+           'valeur' =>  $item['valeur'] 
+          ]);
+   
+         }
+      }
+
+
+
+
+
+
+
+
+      return redirect()->route('GestionnairesStock.index')
+        ->with('success','modifié avec succès');
+    }
+  }
+
+  
+  
+
+  public function display(Request $r)
+  {
+
+      $materiel=Materiel::find($r['id']);
+
+    return view('gestionnairestock.Affiche',['materiel' => $materiel ]);
+  }
+
+
+ 
+
+
+}
